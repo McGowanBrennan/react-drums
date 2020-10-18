ce51c29b3700ee2ac9d2ea79f604289924e11e7f
Brennan McGowan <btm5jg@virginia.edu>
2020-10-18 11:14:48 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Metronome from './Metronome';
+//import Metronome from './Metronome';
 import App from "./App";
 import './index.css';
 
